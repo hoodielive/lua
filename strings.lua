88c3d6954fb9c26afaa4d6ad78d1f3972f37ffa0
hoodielive <hoodielive@gmail.com>
2018-11-30 15:47:25 -0500

    strings and what not

---
+++ strings.lua
@@ -0,0 +1,8 @@
+s = 'I am immutable like Python'
+d = "I have no qualms with double quotes either"
+u = [[
+  "heredoc or multiline strings"
+  "another line"
+  ]]
+
+d = nil #to undefine d - Garbage Collection of sorts
