de166567e06da63900c91115f0a872938c742567
hoodielive <hoodielive@gmail.com>
2022-11-17 10:09:37 -0500

    I quit

---
+++ 2022/main.lua
@@ -0,0 +1,19 @@
+print("Hello Larry " .. "How are you? " .. "This is basic output.")
+
+local affairs = "Sadness"
+
+local a = 9
+
+print(affairs)
+
+local b = a + 5
+print(b)
+
+print(type(b))
+
+print(math.random(3))
+print(math.randomseed(os.time()))
+
+print([[Hello World]])
+
+print([[ Dear God I hate this language ]])
